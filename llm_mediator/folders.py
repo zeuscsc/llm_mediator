ff5a4a2b0029bb6292a53d435c9c8a50c4f78a57
Zeus Chiu <zeuscsc@gmail.com>
2023-07-27 10:26:45 +0800

    renamed models

--- llm_mediator/folders.py
+++ llm_mediator/folders.py
@@ -1,16 +1,16 @@
 import os
 def reset_root_folder(path:str):
     global ROOT_FOLDER,\
-        CACHES_FOLDER,\
+        LLM_CACHES_FOLDER,\
         LLM_RESPONSE_CACHE_FOLDER,\
         LLM_STREAM_RESPONSE_CACHE_FOLDER,\
         LLM_CONVERSATION_STREAM_CACHE_FOLDER,\
-        LLM_FOLDER
+        LLM_MODELS_FOLDER
     ROOT_FOLDER=path
-    CACHES_FOLDER = f"{ROOT_FOLDER}llm_caches"
-    LLM_RESPONSE_CACHE_FOLDER = os.path.join(CACHES_FOLDER,"llm_response_cache")
-    LLM_STREAM_RESPONSE_CACHE_FOLDER = os.path.join(CACHES_FOLDER,"llm_stream_response_cache")
-    LLM_CONVERSATION_STREAM_CACHE_FOLDER = os.path.join(CACHES_FOLDER,"llm_conversation_stream_cache")
-    LLM_FOLDER = f"{ROOT_FOLDER}llm"
+    LLM_CACHES_FOLDER = os.path.join(ROOT_FOLDER,"llm_caches")
+    LLM_RESPONSE_CACHE_FOLDER = os.path.join(LLM_CACHES_FOLDER,"llm_response_cache")
+    LLM_STREAM_RESPONSE_CACHE_FOLDER = os.path.join(LLM_CACHES_FOLDER,"llm_stream_response_cache")
+    LLM_CONVERSATION_STREAM_CACHE_FOLDER = os.path.join(LLM_CACHES_FOLDER,"llm_conversation_stream_cache")
+    LLM_MODELS_FOLDER = os.path.join(ROOT_FOLDER,"llm_models")
 
-reset_root_folder("./")
+reset_root_folder(os.getcwd())
