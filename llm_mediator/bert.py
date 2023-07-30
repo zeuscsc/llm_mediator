6ad73bc63b28c31cdcfa637445a79f5add0ea38d
Zeus Chiu <zeuscsc@gmail.com>
2023-07-30 16:39:11 +0800

    super class fix

--- llm_mediator/bert.py
+++ llm_mediator/bert.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
+from .llm import LLM_Base
 model:SentenceTransformer=None
-class Bert:
+class Bert(LLM_Base):
     def __init__(self):
         if model is None:
             model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
