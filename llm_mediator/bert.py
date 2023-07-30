3faf29402482e9383b4c5c545b6f4ea44fd3b29b
Zeus Chiu <zeuscsc@gmail.com>
2023-07-30 22:10:51 +0800

    bert fix

--- llm_mediator/bert.py
+++ llm_mediator/bert.py
@@ -2,8 +2,23 @@ from sentence_transformers import SentenceTransformer
 from .llm import LLM_Base
 model:SentenceTransformer=None
 class Bert(LLM_Base):
-    def __init__(self):
+    def get_model_name(self):
+        return "Bert"
+    def set_model_name(self,model_name):
+        return
+    
+    def get_embedding(self,sentences:str):
         if model is None:
             model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
-    def get_embedding(self,sentences:str):
         return model.encode(sentences)
+    
+    def get_response(self,system,assistant,user):
+        return
+    def get_response_stream(self,system,assistant,user):
+        return
+    def get_conversation_stream(self,messages):
+        return
+    def get_conversation_response(self,messages):
+        return
+    def detect_if_tokens_oversized(self,e):
+        return False
