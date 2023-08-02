6a19dfb1a910a75013de15d82987e1624b4cbadd
Zeus Chiu <zeuscsc@gmail.com>
2023-08-02 11:20:21 +0800

    average_pool

--- llm_mediator/embedding.py
+++ llm_mediator/embedding.py
@@ -1,9 +1,17 @@
 from transformers import AutoTokenizer, AutoModel
 from .llm import LLM_Base
+from torch import Tensor
+import torch.nn.functional as F
 class Embedding(LLM_Base):
     model_name="intfloat/multilingual-e5-large"
     tokenizer:AutoTokenizer=None
     model:AutoModel=None
+
+    def average_pool(last_hidden_states: Tensor,attention_mask: Tensor) -> Tensor:
+        last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+        return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+
+
     def get_model_name(self):
         return self.model_name
     def set_model_name(self,model_name):
@@ -13,7 +21,10 @@ class Embedding(LLM_Base):
         if self.model is None:
             self.tokenizer = AutoTokenizer.from_pretrained(self.get_model_name())
             self.model = AutoModel.from_pretrained(self.get_model_name())
-        return self.model.encode(sentences)
+        batch_dict = self.tokenizer(sentences, max_length=512, padding=True, truncation=True, return_tensors='pt')
+        outputs = self.model(**batch_dict)
+        embeddings = Embedding.average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+        return F.normalize(embeddings, p=2, dim=1)
     
     def get_response(self,system,assistant,user):
         return
