e0511b7185215f2c71547043807d2002722532b0
Zeus Chiu <zeuscsc@gmail.com>
2023-09-12 14:43:16 +0800

    improved description

--- llm_mediator/parallel_tasks_queuer.py
+++ llm_mediator/parallel_tasks_queuer.py
@@ -3,23 +3,26 @@ import time
 HEADLESS=True
 
 def build_and_execute_tasks(tasks:list[Callable]|Callable,parameters:Iterator,threads_count:int=10,slience=True,waiting_time=0):
+    """Initialize multi-threads queue for parallel tasks
+    Args:
+        tasks (list[Callable]|Callable): Task that is going to be call.
+                            When create your task, please follow the following format.
+                            Example:
+                                def task(key,value):
+                                    return
+                                params=[]
+                                for key in value:
+                                    params.append((key,value))
+                                    or
+                                    params.append({key=key,value=value})
+                                build_and_execute_tasks(task,params)
+        parameters (Iterator): Tasks list parameters.
+        threads_count (int, optional): How many site you want to scrape at a time. Defaults to 10.
+        slience (bool, optional): Stop printing Tasks Running and Done message. Defaults to True.
+        waiting_time (int, optional): Wait number of seconds between each threads start. Defaults to 0.
+    """
     return build_and_execute(parameters,tasks,threads_count,slience,waiting_time)
 def build_and_execute(parameters:Iterator,tasks:list[Callable]|Callable,threads_count:int=10,slience=True,waiting_time=0):
-    """Initialize multi-threads queue for parallel tasks
-
-    Args:
-        parameters (Iterator): Tasks list parameters.
-
-        task (Callable): Task that is going to be call.
-                            When create your task, please follow the following format.
-                            task(on_checkpoint_reached:Callable[[list],None],arg1,arg2,arg3...)
-
-        threads_count (int, optional): How many site you want to scrape at a time. Defaults to 10.
-
-        slience (bool, optional): Stop printing Tasks Running and Done message. Defaults to True.
-
-        waiting_time (int, optional): Wait number of seconds between each threads start. Defaults to 1.
-    """
     def run_task(*args, **kwargs):
         try:
             if isinstance(tasks,list):
